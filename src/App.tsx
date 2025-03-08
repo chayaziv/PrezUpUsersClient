d11b1a16e1f7c389085df7c39319f2a3f88e397c
chayaziv <chy15658@gmail.com>
2025-03-08 23:11:21 +0200

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { RouterProvider } from 'react-router'
 import './App.css'
+import { router } from './Router'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    
-      <></>
+
+  return (  
+      <>
+       <RouterProvider router={router} />;
+      </>
   )
 }
 
