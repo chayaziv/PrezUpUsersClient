c22ee757271f9feeb45ab5863fcd2628f947804a
chayaziv <chy15658@gmail.com>
2025-03-12 11:23:41 +0200

    add types

---
+++ src/types/presentation.ts
@@ -0,0 +1,17 @@
+export interface PresentationType {
+    id: number;
+    fileUrl: string;
+    clarity: number;
+    clarityFeedback: string;
+    fluency: number;
+    fluencyFeedback: string;
+    confidence: number;
+    confidenceFeedback: string;
+    engagement: number;
+    engagementFeedback: string;
+    speechStyle: number;
+    speechStyleFeedback: string;
+    score: number;
+    tips: string;
+  }
+  
---
+++ src/types/user.ts
@@ -0,0 +1,12 @@
+export interface UserType {
+    name: string;
+    email: string;
+    password: string;
+    jobTitle: string;
+    company: string;
+    yearsOfExperience: number;
+    compareWithOthers: boolean;
+    allowPublicPresentations: boolean;
+    accountStatus: string;
+  }
+  
